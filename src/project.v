fde5e473ac61864e954a64c34545c4789ec5489d
Tholin <luca.horn@gmx.de>
2025-06-08 07:16:02 +0200

    Update project.v

--- src/project.v
+++ src/project.v
@@ -18,10 +18,20 @@ module tt_um_example (
 
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  assign uio_out = lfsr;
+  assign uio_oe  = 8'hFF;
 
+    reg [7:0] lfsr;
+    
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
+    
+    always @(posedge clk) begin
+        if(!rst_n) begin
+            lfsr <= 1;
+        end else begin
+            lfsr <= {lfsr[6:0], lfsr[7] ^ lfsr[5] ^ lfsr[4] ^ lfsr[3]};
+        end
+    end
 
 endmodule
