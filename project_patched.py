d49e806b8de8a77cd58a3626b4993f7b6f616506
Tholin <luca.horn@gmx.de>
2025-06-08 08:31:12 +0200

    Try running setup on its own

--- project_patched.py
+++ project_patched.py
@@ -580,9 +580,10 @@ class Project:
             os.makedirs("runs/wokwi", exist_ok=True)
             arg_progress = "--hide-progress-bar" if "CI" in os.environ else ""
             arg_pdk_root = '--pdk-root "$PDK_ROOT"' if "PDK_ROOT" in os.environ else ""
-            setup_cmd = f"python -m openlane.steps run --help"
+            setup_cmd = f"python -m openlane.steps run setup --pdk-root {arg_pdk_root} --docker-no-tty --dockerized --run-tag wokwi --force-run-dir runs/wokwi {arg_progress} src/config_merged.json"
             harden_cmd = f"python -m openlane {arg_pdk_root} --docker-no-tty --dockerized --run-tag wokwi --force-run-dir runs/wokwi {arg_progress} src/config_merged.json"
             env = os.environ.copy()
+        logging.info(f"The setup CMD is: {setup_cmd}")
         logging.info(f"The harden CMD is: {harden_cmd}")
         p = subprocess.run(setup_cmd, shell=True, env=env)
         if p.returncode != 0:
